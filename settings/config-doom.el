e47dd3837247cd80c8ef8e0ea27c6c94b7e4a295
Nicholas Martin <nmartin84@gmail.com>
2019-12-29 14:37:36 -0600

    removed pitch-font

--- settings/config-doom.el
+++ settings/config-doom.el
@@ -3,5 +3,4 @@
 (setq doom-theme 'doom-city-lights)
 
 (setq doom-font (font-spec :family "Source Code Pro" :size 22)
-      doom-big-font (font-spec :family "Source Code Pro" :size 28)
-      doom-variable-pitch-font (font-spec :family "Fira Code"))
+      doom-big-font (font-spec :family "Source Code Pro" :size 28))
