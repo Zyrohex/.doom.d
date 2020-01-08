b537683ed76b924bcbba69836b6c43caceae948e
Nicholas Martin <nmartin84@gmail.com>
2020-01-07 22:40:37 -0600

    capture template updates

--- config-orgmode_capture.el
+++ config-orgmode_capture.el
@@ -13,6 +13,7 @@
 :PROPERTIES:
 :CREATED:    %U
 :END:
+
 ** notes
 %?"))
 (add-to-list 'org-capture-templates
@@ -21,7 +22,7 @@
 :PROPERTIES:
 :END:
 :RESOURCES:
-- [[%^{link}][%^{description}]]
+[[%^{url}]]
 :END:
 
 *Requirements*:
@@ -37,7 +38,7 @@
 :END:
 
 *links*:
-
+[[%^{url}]]
 
 *next steps*:
 - [ ] %^{next steps}
@@ -54,31 +55,25 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'org-capture-templates
              '("rr" "Reference"  entry (file"~/.gtd/references.org")
-               "* %^{description}\n\n*links*:\n%^L\n\n%?"))
+"* %^{description}
+:RESOURCES:
+[[%^{url}]]
+:END:
+
+%?"))
 (add-to-list 'org-capture-templates
-             '("rs" "Reference with code block" entry (file"~/.gtd/references.org")
-               "* %^{description}\n#+BEGIN_SRC %^{lang}\n%x\n#+END_SRC\n:END:\n%?"))
+             '("rs" "Reference + dump clipboard to srcblock" entry (file"~/.gtd/references.org")
+"* %^{description}
+
+#+BEGIN_SRC %^{lang}
+%x
+#+END_SRC
+
+%?"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                            Data Trackers                                                               ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(add-to-list 'org-capture-templates
-             '("tb" "Big Purchases" table-line (file+headline"~/.gtd/data-tracker.org" "Big Purchases")
-               "|  | %^{itemname} | %^{amount} | %^{why?} |" :table-line-pos "II-1" :immediate-finish t))
-
-(add-to-list 'org-capture-templates
-             '("ts" "Sugar Intake" table-line (file+headline"~/.gtd/data-tracker.org" "Sugar Intake")
-               "|  | %u | %^{itemname} | %^{sugar} |" :table-line-pos "II-1" :immediate-finish t))
-
-(add-to-list 'org-capture-templates
-             '("tc" "Calories Burned" entry (file+headline"~/.gtd/data-tracker.org" "Calories Burned")
-"* [%u] [%\\2] [%\\3] [%\\4]
-:PROPERTIES:
-:ACTIVITY: %^{activity}
-:CALORIES: %^{calories}
-:DISTANCE: %^{distance}
-:END" :immediate-finish t))
 
 (add-to-list 'org-capture-templates
              '("dt" "Journal Thoughs" entry (file+datetree"~/.gtd/journal.org")
