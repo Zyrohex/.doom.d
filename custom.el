51f219be6a57de55783b8bef9808aaac9c32530a
nick martin <nmartin84@gmail.com>
2020-11-19 20:56:35 -0600

    updates

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/.org/gtd/todo.org" "~/.org/gtd/archives.org" "~/.org/gtd/astronomy.org" "~/.org/gtd/biology.org" "~/.org/gtd/coding.org" "~/.org/gtd/emacs.org" "~/.org/gtd/journal.org" "~/.org/gtd/personal.org" "~/.org/gtd/techie.org" "~/.org/gtd/slides/202010091005-orgmode.org" "~/.org/gtd/slides/testslide.org"))
+ '(package-selected-packages '(tao-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
