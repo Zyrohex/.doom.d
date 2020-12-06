bafb85fd65c5783c9d7831de4213490229457dec
nick martin <nmartin84@gmail.com>
2020-12-05 22:01:33 -0600

    commented out unused packages

--- packages.el
+++ packages.el
@@ -2,28 +2,32 @@
 (package! org-roam)
 (package! company-org-roam)
 (package! org-roam-server)
+
 ;; Alphapapa packages
 (package! helm-org-rifle)
 (package! org-super-agenda)
-(package! visual-fill-column)
+;(package! visual-fill-column)
 (package! org-sidebar)
 (package! org-ql)
 (package! org-web-tools)
+
 ;; Themes
 (package! chocolate-theme)
-(unpin! doom-themes)
+
 ;; Orgmode Packages
-(package! org-analyzer)
+;(package! org-analyzer)
 (package! org-edna)
-(package! literate-calc-mode)
-(package! ftable
-  :recipe (:host github :repo "casouri/ftable"))
+;(package! literate-calc-mode)
+;(package! ftable
+;  :recipe (:host github :repo "casouri/ftable"))
+
 ;; Python
 (package! ob-ipython)
 (package! anaconda-mode)
 (package! company-anaconda)
+
 ;; Misc Packages
-(package! company-manually)
+;(package! company-manually)
 (package! dimmer)
 (package! elfeed)
 (package! elfeed-org)
