728d95d69830c0775eb5d98ad6bcf81a44c533c2
nick martin <nmartin84@gmail.com>
2021-01-10 18:32:35 -0600

    + ipython -smartparens(-> org-roam-server)

--- init.el
+++ init.el
@@ -141,7 +141,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +dragndrop +jupyter +pandoc +present +gnuplot)               ; organize your plain life in plain text
+       (org +roam +dragndrop +jupyter +ipython +pandoc +present +gnuplot)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -176,4 +176,4 @@
 
        :config
        literate
-       (default +bindings +smartparens))
+       (default +bindings))
