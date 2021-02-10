43766f89a7d66cb8177fb33401ae58f0cdbaea8f
nick martin <nmartin84@gmail.com>
2021-02-10 15:39:39 -0600

    +pretty mode

--- init.el
+++ init.el
@@ -144,7 +144,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +noter +roam +pomodoro +dragndrop +jupyter +pandoc +present +gnuplot)               ; organize your plain life in plain text
+       (org +pretty +noter +roam +pomodoro +dragndrop +jupyter +pandoc +present +gnuplot)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
